d40bb5a441fdd9b2af707ac7537a5bb2fd673c53
popudev <phuthinh53.it@gmail.com>
2022-09-30 13:17:26 +0700

    login social medio logic new p7

--- routes/auth.js
+++ routes/auth.js
@@ -9,6 +9,7 @@ router.post('/register', AuthenController.register);
 router.post('/login', AuthenController.login);
 router.post('/login/google', AuthenController.loginGoogle);
 router.post('/login/github', AuthenController.loginGithub);
+router.post('/login/facebook', AuthenController.loginFacebook);
 router.get('/refreshToken', AuthenController.requestRefreshToken);
 router.get('/logout', AuthenController.logout);
 router.get('/github/callback', AuthenController.githubCallback);
