6b95e482c6f878dfcb7bff9d176e8636a23467ee
Phu Thinh <thinhcp@ggroup.vn>
2023-09-18 21:47:01 +0700

    fix: error regex

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -54,7 +54,7 @@ const ProductController = {
       const match = {
         title: {
           $regex: req.query.title || '',
-          $options: 'gi',
+          $options: 'i',
         },
       };
 
